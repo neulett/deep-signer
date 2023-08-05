712adae17bcfde3584a955a434d8aae3be84564f
neulett <neulet.git@gmail.com>
2023-08-05 11:32:44 +0900

    npy_exporter 경로 수정

--- keypoint_extraction/npy_exporter.py
+++ keypoint_extraction/npy_exporter.py
@@ -2,15 +2,15 @@ import numpy as np
 import pandas as pd
 import os
 
-path = './keypoints_save/'
-df = pd.read_excel('../KETI-2017-SL-Annotation-v2_1.xlsx')
+path = 'C:/Users/ILIXYENILXY/Desktop/deep_signer/local_dir/keypoints_data'
+df = pd.read_excel('C:/Users/ILIXYENILXY/Desktop/deep_signer/local_dir/KETI-2017-SL-Annotation-v2_1.xlsx')
 df = df.sort_values('번호')
 
 def create_array(size, value):
     arr = np.array([value] * size)
     return arr
 
-label_path = './label_path/'
+label_path = 'C:/Users/ILIXYENILXY/Desktop/deep_signer/local_dir/label_path'
 emp_lst = []
 label = np.array(emp_lst)
 
@@ -19,7 +19,7 @@ for filename in os.listdir(path):
     file_path = os.path.join(path, filename)
     array = np.load(file_path)
 
-    get_index = int(filename[13:18]) - 1    # csv열에 맞게끔 인덱싱번호 생성
+    get_index = int(filename[13:18])   # csv열에 맞게끔 인덱싱번호 생성
 
     result = df.iloc[get_index]     # get_index를 통한 열 인덱싱
     fill_val = result['한국어']     # 같은 열의 라벨 인덱싱
@@ -27,7 +27,7 @@ for filename in os.listdir(path):
     length_array = array.shape[0]   # 
     if length_array > 0:
         arr = create_array(length_array, fill_val)
-        np.save(f'./label_path/{file_name}_label', arr)
+        np.save(f'{label_path}/{file_name}_label', arr)
     else:
         break
 
