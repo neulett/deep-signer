dd034441ac8b5ab03476f3f15dc7b1a7da4518fd
neulett <neulet.git@gmail.com>
2023-09-18 02:54:19 +0900

    extractor에 data 전처리 코드 통합

--- data_preprocess/main.py
+++
@@ -1,9 +0,0 @@
-from keypoints_extraction import Models
-
-source_path = input("Source Path : ")
-dest_path = input("Dest Path : ")
-
-selected_model = input("Select Model : ")
-
-extractor = Models(source_path, dest_path)
-extractor.working_threads(model=selected_model)
