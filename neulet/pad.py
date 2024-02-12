4b06e69b8628e2df589b124aa48effc6264b72dd
neulett <neulet.git@gmail.com>
2024-02-12 22:53:54 +0900

    code refectored & argparser => settings.yaml optional 대체

--- neulet/pad.py
+++ neulet/pad.py
@@ -1,79 +1,73 @@
 import os
 import cv2
 import math
-import argparse
 import numpy as np
+
+from utils import Utils
 from tqdm.auto import tqdm
 from concurrent.futures import ThreadPoolExecutor
 
 class engine:
     def __init__(self):
-        self.default_source_path = "../local/source"
-        self.default_pad_source_path = '../local/pad_source'
-        self.file_list = []
-        self.default_H = 800
-        self.default_W = 600
+        opts = Utils.open_settings_yaml()
+        self.FPS = opts['setting']['']
+        self.HEIGHT = opts['setting']['height']
+        self.WIDTH = opts['setting']['width']
+        self.WORKERS = opts['setting']['workers']
+        self.SOURCE_PATH = opts['setting']['src_path']
+        self.PADDED_SAVE_PATH = opts['setting']['padded_path']
         self.max_duration = 0
 
     def get_max_duration(self):
-        max_file = ''
-        self.file_list = os.listdir(SOURCE)
-
-        for filename in self.file_list:
-            file_path = os.path.join(SOURCE, filename)
-            cap = cv2.VideoCapture(file_path)
-
-            frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-            frame_rate = int(cap.get(cv2.CAP_PROP_FPS))
-            duration = math.ceil(frame_count // frame_rate)  # duration = frame_count // frame_rate
+        for filename in tqdm(os.listdir(self.SOURCE_PATH), total=len(os.listdir(self.SOURCE_PATH))):
+            full_filename = os.path.join(self.SOURCE_PATH, filename)
+            cap = cv2.VideoCapture(full_filename)
+            duration = math.ceil(cap.get(cv2.CAP_PROP_FRAME_COUNT) // cap.get(cv2.CAP_PROP_FPS))
 
             if duration >= self.max_duration:
                 self.max_duration = duration
                 max_file = filename
 
-        return self.max_duration, max_file
+        print(f"Max duration - {self.max_duration} sec - {max_file}")
+        return self.max_duration
 
-    def process_video(self, filename):
-        file_path = os.path.join(SOURCE, filename)
+    def processing_src(self, filename):
+        file_path = os.path.join(self.SOURCE_PATH, filename)
         cap = cv2.VideoCapture(file_path)
-        output_path = os.path.join(DEST, filename)
+        output_path = os.path.join(self.PADDED_SAVE_PATH, filename)
         fourcc = cv2.VideoWriter.fourcc(*'mp4v')
-        output = cv2.VideoWriter(output_path, fourcc, FPS, (HEIGHT, WIDTH))
+        output = cv2.VideoWriter(output_path, fourcc, self.FPS, (self.HEIGHT, self.WIDTH))
+
+        current_duration =  math.ceil(cap.get(cv2.CAP_PROP_FRAME_COUNT) // cap.get(cv2.CAP_PROP_FPS))
+        target_padding_frame = self.max_duration - current_duration
 
         while True:
             ret, frame = cap.read()
             if not ret:
                 break
 
-            frame = cv2.resize(frame, (HEIGHT, WIDTH))
+            frame = cv2.resize(frame, (self.HEIGHT, self.WIDTH))
             output.write(frame)
 
-    def padding_src(self):
-        print(f"Pad workthreads start.")
-        if HEIGHT == 0 or WIDTH == 0:
-            raise ValueError(f"Source resize ValueError {HEIGHT},{WIDTH}")
+        for _ in range(target_padding_frame):
+            output.write(frame)
 
-        with ThreadPoolExecutor(max_workers=WORKERS) as executor:
-            list(tqdm(executor.map(self.process_video, self.file_list), total=len(self.file_list), desc="Padding Progress"))
+        cap.release()
+        output.release()
+
+    def run_threads(self):
+        self.get_max_duration()
+        executor = ThreadPoolExecutor(max_workers=self.WORKERS)
+        futures = []
+
+        for filename in tqdm(os.listdir(self.SOURCE_PATH), total=len(os.listdir(self.SOURCE_PATH))):
+            future = executor.submit(self.processing_src, filename)
+            futures.append(future)
+
+        for future in tqdm(futures, total=len(futures)):
+            future.result()
+
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-src', '--source', default='../video/source/', type=str)
-    parser.add_argument('-dest', '--dest', default='../video/padded_source/', type=str)
-    parser.add_argument('-height', '--height', default=800, type=int)
-    parser.add_argument('-width', '--width', default=600, type=int)
-    parser.add_argument('-fps', '--fps', default=30.0, type=float)
-    parser.add_argument('-workers', '--workers', type=int)
-    args = parser.parse_args()
-
-    SOURCE = args.source
-    DEST = args.dest
-    HEIGHT = args.height
-    WIDTH = args.width
-    FPS = args.fps
-    WORKERS = args.workers
-
     instance = engine()
-    max_duration, max_file = instance.get_max_duration()
-    print(f"Max duration - {max_duration} sec - {max_file}")
-    instance.padding_src()
+    instance.run_threads()
