18faa104c514f44467514efb56553a27920ea968
neulett <neulet.git@gmail.com>
2024-02-13 01:26:46 +0900

    code refectored & argparser change to settings.yaml

--- neulet/extractor.py
+++ neulet/extractor.py
@@ -4,6 +4,7 @@ import argparse
 import numpy as np
 import mediapipe as mp
 
+from utils import Utils
 from tqdm.auto import tqdm
 from concurrent.futures import ThreadPoolExecutor
 
@@ -11,19 +12,25 @@ class Engine:
     def __init__(self):
         self.keypoints = []
         self.keypoints_list = []
-        self.full_filename = []
+        opts = Utils.open_settings_yaml()
+        self.MODEL = opts['setting']['model']
+        self.HEIGHT = opts['setting']['height']
+        self.WIDTH = opts['setting']['width']
+        self.WORKERS = opts['setting']['workers']
+        self.SOURCE_PATH = opts['setting']['src_path']
+        self.FEATURE_SAVE_PATH = opts['setting']['keypoints_path']
 
     def extract_keypoints(self):
-        for filename in tqdm(os.listdir(SOURCE), total=len(os.listdir(SOURCE))):
-            full_filename = os.path.join(SOURCE, filename)
+        for filename in tqdm(os.listdir(self.SOURCE_PATH), total=len(os.listdir(self.SOURCE_PATH))):
+            full_filename = os.path.join(self.SOURCE_PATH, filename)
             cap = cv2.VideoCapture(full_filename)
 
-            if MODEL == "pose":
+            if self.MODEL == "pose":
                 solution = mp.solutions.pose
-            elif MODEL == "holistic":
+            elif self.MODEL == "holistic":
                 solution = mp.solutions.holistic
             else:
-                raise ValueError("Invalid Model")
+                raise ValueError("Invalid self.MODEL")
 
             with solution(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
 
@@ -35,14 +42,14 @@ class Engine:
                     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
                     results = pose.process(image_rgb)
 
-                    if MODEL == "pose" and results.pose_landmarks:
+                    if self.MODEL == "pose" and results.pose_landmarks:
                         for landmark in results.pose_landmarks.landmark:
                             self.keypoints.append([landmark.x, landmark.y, landmark.z, landmark.visibility])
                         self.keypoints_list.append(self.keypoints)
 
-                        np.save(f"{DEST}/{os.path.splitext(os.path.basename(filename))[0]}_pose.npy", self.keypoints_list) 
+                        np.save(f"{self.FEATURE_SAVE_PATH}/{os.path.splitext(os.path.basename(filename))[0]}_pose.npy", self.keypoints_list) 
 
-                    elif MODEL == "holistic" and (results.face_landmarks or
+                    elif self.MODEL == "holistic" and (results.face_landmarks or
                                                   results.left_hand_landmarks or
                                                   results.right_hand_landmarks or
                                                   results.pose_landmarks):
@@ -76,32 +83,20 @@ class Engine:
                                 ])
 
                         self.keypoints_list.append(keypoints)
-                        np.save(f"{DEST}/{os.path.splitext(os.path.basename(filename))[0]}_holistic.npy", self.keypoints_list)
+                        np.save(f"{self.FEATURE_SAVE_PATH}/{os.path.splitext(os.path.basename(filename))[0]}_holistic.npy", self.keypoints_list)
 
                 cap.release()
 
     def run_threads(self):
-        executor = ThreadPoolExecutor(max_workers=WORKERS)
+        executor = ThreadPoolExecutor(max_workers=self.WORKERS)
         futures = []
 
-        future = executor.submit(self.extract_keypoints, self.full_filename, DEST)
+        future = executor.submit(self.extract_keypoints, self.full_filename, self.FEATURE_SAVE_PATH)
         futures.append(future)
 
         for future in tqdm(futures, total=len(futures)):
             future.result()
         
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-model', '--model', default= None, type=str)
-    parser.add_argument('-src', '--source', default='../local/pad_source', type=str)
-    parser.add_argument('-dest', '--dest', default='../local/keypoints', type=str)
-    parser.add_argument('-workers', '--workers', default=4, type=int)
-    args = parser.parse_args()
-
-    MODEL = args.model
-    SOURCE = args.source
-    DEST = args.dest
-    WORKERS = args.workers
-
     instance = Engine()
     instance.run_threads()
