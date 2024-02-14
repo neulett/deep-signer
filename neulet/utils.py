13c1bb84641ef51f950b7c41a8d9d266de308320
neulett <neulet.git@gmail.com>
2024-02-14 22:25:19 +0900

    ray 병렬처리 적용 & run_threads_ray 작업 반복문 tqdm 제거

--- neulet/utils.py
+++ neulet/utils.py
@@ -1,5 +1,6 @@
 import os
 import cv2
+import ray
 import math
 import yaml
 import numpy as np
@@ -24,75 +25,76 @@ class Utils:
         self.PADDED_SAVE_PATH = opts['path']['padded_path']
         
     @classmethod
-    def open_settings_yaml(self, path='./command.yaml'):
+    def open_settings_yaml(self, path='../command.yaml'):
         with open(path) as f:
             opts = yaml.load(f, Loader=yaml.FullLoader)
             return opts
 
+    @ray.remote
     def extract_keypoints(self, filename):
-        full_filename = os.path.join(self.PADDED_SAVE_PATH, filename)
-        cap = cv2.VideoCapture(full_filename)
+            full_filename = os.path.join(self.PADDED_SAVE_PATH, filename)
+            cap = cv2.VideoCapture(full_filename)
 
-        if self.MODEL == "pose":
-            solution = mp.solutions.pose.Pose
-        elif self.MODEL == "holistic":
-            solution = mp.solutions.holistic.Holistic
-        else:
-            raise ValueError("Invalid Model")
-        with solution(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+            if self.MODEL == "pose":
+                solution = mp.solutions.pose.Pose
+            elif self.MODEL == "holistic":
+                solution = mp.solutions.holistic.Holistic
+            else:
+                raise ValueError("Invalid Model")
+            with solution(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
 
-                while True:
-                    opened, image = cap.read()
-                    if not opened:
-                        break
+                    while True:
+                        opened, image = cap.read()
+                        if not opened:
+                            break
 
-                    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-                    results = pose.process(image_rgb)
+                        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                        results = pose.process(image_rgb)
 
-                    if self.MODEL == "pose" and results.pose_landmarks:
-                        for landmark in results.pose_landmarks.landmark:
-                            self.keypoints.append([landmark.x, landmark.y, landmark.z, landmark.visibility])
-                        self.keypoints_list.append(self.keypoints)
-
-                        np.save(f"{self.FEATURE_SAVE_PATH}/{os.path.splitext(os.path.basename(filename))[0]}_pose.npy", self.keypoints_list) 
-
-                    elif self.MODEL == "holistic" and (results.face_landmarks or
-                                                  results.left_hand_landmarks or
-                                                  results.right_hand_landmarks or
-                                                  results.pose_landmarks):
-                        keypoints = {'face': [],
-                                     'left_hand': [],
-                                     'right': [],
-                                     'pose': []}
-                        
-                        if results.pose_landmarks:
+                        if self.MODEL == "pose" and results.pose_landmarks:
                             for landmark in results.pose_landmarks.landmark:
-                                keypoints['pose'].append([
-                                    landmark.x, landmark.y, landmark.z, landmark.visibility
-                                ])
+                                self.keypoints.append([landmark.x, landmark.y, landmark.z, landmark.visibility])
+                            self.keypoints_list.append(self.keypoints)
 
-                        if results.face_landmarks:
-                            for landmark in results.face_landmarks.landmark:
-                                keypoints['face'].append([
-                                    landmark.x, landmark.y, landmark.z, landmark.visibility
-                                ])
+                            np.save(f"{self.FEATURE_SAVE_PATH}/{os.path.splitext(os.path.basename(filename))[0]}_pose.npy", self.keypoints_list) 
 
-                        if results.left_hand_landmarks:
-                            for landmark in results.left_hand_landmarks.landmark:
-                                keypoints['left_hand'].append([
-                                    landmark.x, landmark.y, landmark.z, landmark.visibility
-                                ])
+                        elif self.MODEL == "holistic" and (results.face_landmarks or
+                                                      results.left_hand_landmarks or
+                                                      results.right_hand_landmarks or
+                                                      results.pose_landmarks):
+                            keypoints = {'face': [],
+                                         'left_hand': [],
+                                         'right': [],
+                                         'pose': []}
 
-                        if results.right_hand_landmarks:
-                            for landmark in results.right_hand_landmarks.landmark:
-                                keypoints['right_hand'].append([
-                                    landmark.x, landmark.y, landmark.z, landmark.visibility
-                                ])
+                            if results.pose_landmarks:
+                                for landmark in results.pose_landmarks.landmark:
+                                    keypoints['pose'].append([
+                                        landmark.x, landmark.y, landmark.z, landmark.visibility
+                                    ])
 
-                        self.keypoints_list.append(keypoints)
-                        np.save(f"{self.FEATURE_SAVE_PATH}/{os.path.splitext(os.path.basename(filename))[0]}_holistic.npy", self.keypoints_list)
+                            if results.face_landmarks:
+                                for landmark in results.face_landmarks.landmark:
+                                    keypoints['face'].append([
+                                        landmark.x, landmark.y, landmark.z, landmark.visibility
+                                    ])
 
-                cap.release()
+                            if results.left_hand_landmarks:
+                                for landmark in results.left_hand_landmarks.landmark:
+                                    keypoints['left_hand'].append([
+                                        landmark.x, landmark.y, landmark.z, landmark.visibility
+                                    ])
+
+                            if results.right_hand_landmarks:
+                                for landmark in results.right_hand_landmarks.landmark:
+                                    keypoints['right_hand'].append([
+                                        landmark.x, landmark.y, landmark.z, landmark.visibility
+                                    ])
+
+                            self.keypoints_list.append(keypoints)
+                            np.save(f"{self.FEATURE_SAVE_PATH}/{os.path.splitext(os.path.basename(filename))[0]}_holistic.npy", self.keypoints_list)
+
+                    cap.release()
 
     def get_max_duration(self, filename):
         full_filename = os.path.join(self.SOURCE_PATH, filename)
@@ -105,7 +107,7 @@ class Utils:
 
         print(f"Max duration - {self.max_duration} sec - {max_file}")
         return self.max_duration
-    
+    @ray.remote
     def processing_padding_src(self, filename):
         file_path = os.path.join(self.SOURCE_PATH, filename)
         cap = cv2.VideoCapture(file_path)
@@ -137,9 +139,16 @@ class Utils:
             solution = self.extract_keypoints
         else:
             raise ValueError("Invalid Worktype. please, check worktype in yaml.")
+        
+        ray.init(num_cpus=self.WORKERS)  
+        futures = []
 
-        for filename in tqdm(os.listdir(self.SOURCE_PATH), total=len(os.listdir(self.SOURCE_PATH))):
-            solution(filename)
+        for filename in os.listdir(self.SOURCE_PATH):
+            futures.append(solution.remote(self, filename))
+
+        ray.get(futures)  
+        ray.shutdown()
+
 
 if __name__ == '__main__':
     instance = Utils()
